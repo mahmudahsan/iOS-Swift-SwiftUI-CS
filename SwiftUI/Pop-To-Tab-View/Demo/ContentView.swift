b033b96d2b601fbfb892466a005c32055bbf2438
Mahmud Ahsan <mahmud@thinkdiff.net>
2020-04-12 23:48:30 +0800

    Add pop to tabview swiftui project

---
+++ SwiftUI/Pop-To-Tab-View/Demo/ContentView.swift
@@ -0,0 +1,115 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by mahmud on 8/4/20.
+//  Copyright © 2020 iThinkdiff. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var appState: AppState
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // TabView1()
+                tabsView
+            }
+        }
+    }
+    
+    var tabsView: some View {
+        TabView {
+            TabView1()
+            .tabItem({
+                Image(systemName: "flame")
+                Text("Fire")
+            }).tag(1)
+            
+            TabView2()
+                .tabItem({
+                    Image(systemName: "umbrella")
+                    Text("Rain")
+                }).tag(2)
+        }
+    }
+}
+
+// MARK:- Tab View 1
+struct TabView1: View {
+    @EnvironmentObject var appState: AppState
+    @State var isView1Active: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text("Tab View 1")
+                .font(.headline)
+            NavigationLink(destination: View1(), isActive: $isView1Active) {
+                Text("View 1")
+                    .font(.headline)
+            }
+            .isDetailLink(false)
+        }
+        .onReceive(self.appState.$moveToDashboard) { moveToDashboard in
+            if moveToDashboard {
+                self.isView1Active = false
+                self.appState.moveToDashboard = false
+                print("Move to Dashboard")
+            }
+        }
+    }
+}
+
+// MARK:- Tab View 2
+struct TabView2: View {
+    
+    var body: some View {
+        VStack {
+            Text("Tab View 2")
+                .font(.headline)
+        }
+    }
+}
+
+// MARK:- View 1
+struct View1: View {
+    var body: some View {
+        VStack {
+            Text("View 1")
+                .font(.headline)
+            NavigationLink(destination: View2()) {
+                Text("View 2")
+                    .font(.headline)
+            }
+        }
+    }
+}
+
+// MARK:- View 2
+struct View2: View {
+    @EnvironmentObject var appState: AppState
+    
+    var body: some View {
+        VStack {
+            Text("View 2")
+                .font(.headline)
+            Button(action: {
+                // FIXME:- add move to dashboard functionality
+                // self.appState.moveToDashboard = true
+                self.appState.reloadDashboard()
+            }) {
+                Text("Move to Dashboard")
+                .font(.headline)
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Pop-To-Tab-View/Demo/States/AppState.swift
@@ -0,0 +1,18 @@
+//
+//  AppState.swift
+//  Demo
+//
+//  Created by mahmud on 11/4/20.
+//  Copyright © 2020 iThinkdiff. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+class AppState: ObservableObject {
+    @Published var moveToDashboard: Bool = false
+    
+    func reloadDashboard() {
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.reloadDashboard()
+    }
+}
